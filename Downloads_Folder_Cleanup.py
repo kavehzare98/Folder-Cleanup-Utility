157c95fd9500204ab65061ed51ca7abdc0194fd5
kavehzare98 <66182975+kavehzare98@users.noreply.github.com>
2023-12-28 10:34:02 -0800

    Provding Documentation
    Providing documentation through comments

--- Downloads_Folder_Cleanup.py
+++ Downloads_Folder_Cleanup.py
@@ -1,7 +1,9 @@
+# Import necessary modules
 import os
 import collections
 from pprint import pprint
 
+# Define file extension categories
 EXT_AUDIO = ['mp3','wav','wma','mid','midi']
 EXT_VIDEO = ['mp4', 'mpg', 'mpeg', 'avi','mov','flv','mkv', 'mwv', 'm4v', 'h264']
 EXT_IMGS = ['png','jpg','JPG','jpeg','gif','svg','bmp','psd','tiff','tif','HEIC']
@@ -27,32 +29,42 @@ files_list = os.listdir(DOWNLOADS_PATH)
 
 # Splitting file name from extension
 for file_name in files_list:
+    # Ignore hidden files
     if file_name[0] != ".":
+        # Extract file extension
         file_ext = file_name.split(".")[-1]
         files_mapping[file_ext].append(file_name)
 
+# Pretty print the files mapping
 pprint(files_mapping)
 
 # Move all files given a file extension to a target directory
 for f_ext, f_list in files_mapping.items():
     if f_ext in EXT_INSTL:
+        # Move installer files to the 'Executables' directory
         for file in f_list:
             os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(BASE_PATH, 'Executables', file))
     elif f_ext in EXT_AUDIO:
+        # Move audio files to the 'Music' directory
         for file in f_list:
             os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(BASE_PATH, 'Music', file))
     elif f_ext in EXT_PROG:
+        # Move programming files to the 'Programming' directory
         for file in f_list:
             os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(BASE_PATH, 'Programming', file))
     elif f_ext in EXT_DOCS or f_ext in EXT_COMPR:
+        # Move document and compressed files to the 'Documents' directory
         for file in f_list:
             os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(BASE_PATH, 'Documents', file))
     elif f_ext in EXT_IMGS:
+        # Move image files to the 'Pictures' directory
         for file in f_list:
             os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(BASE_PATH, 'Pictures', file))
     elif f_ext in EXT_VIDEO:
+        # Move video files to the 'Movies' directory
         for file in f_list:
             os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(BASE_PATH, 'Movies', file))
     else:
+        # Move other files to the 'Other' directory
         for file in f_list:
             os.rename(os.path.join(DOWNLOADS_PATH, file), os.path.join(BASE_PATH, 'Other', file))
