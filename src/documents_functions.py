072e25f9c611bbe28cd2fcc0ad0f447755532dab
kavehzare98 <66182975+kavehzare98@users.noreply.github.com>
2023-12-30 13:01:29 -0800

    update documents functions file

--- src/documents_functions.py
+++ src/documents_functions.py
@@ -1,5 +1,4 @@
 # documents_functions.py
-
 import os
 import collections
 
@@ -11,16 +10,34 @@ ext_dict = {'Text': ['txt'],
             'Presentations': ['ppt', 'pptx'],
             'Compressed Files': ['zip', 'z', 'tar', 'pkg', 'deb']}
 
-# Function to create directories in a given base path
 def createDirectories(destination_directories, base_path):
+    """
+    Create directories in the specified base path if they do not exist.
+
+    Parameters:
+    - destination_directories (list): List of directory names to be created.
+    - base_path (str): Base path where the directories will be created.
+
+    Returns:
+    None
+    """
     for dir in destination_directories:
         dir_path = os.path.join(base_path, dir)
         if not os.path.isdir(dir_path):
             os.mkdir(dir_path)
             print(f"Created a directory for '{dir}'")
 
-# Function to map files in a folder based on their file extension
 def mapFileExtensions(base_path):
+    """
+    Map files in a folder based on their file extensions.
+
+    Parameters:
+    - base_path (str): The path of the folder to be analyzed.
+
+    Returns:
+    files_mapping (defaultdict): A dictionary with file extensions as keys
+                                 and lists of corresponding file names as values.
+    """
     files_mapping = collections.defaultdict(list)
     files_list = os.listdir(base_path)
 
@@ -33,8 +50,17 @@ def mapFileExtensions(base_path):
 
     return files_mapping
 
-# Function to move files to their respective folders based on file extension
 def moveFiles(map, base_path):
+    """
+    Move files to their respective folders based on file extension.
+
+    Parameters:
+    - map (defaultdict): Dictionary with file extensions and corresponding file names.
+    - base_path (str): The base path where the files are located.
+
+    Returns:
+    None
+    """
     for f_ext, f_list in map.items():
         for key in ext_dict.keys():
             if f_ext in ext_dict[key]:
