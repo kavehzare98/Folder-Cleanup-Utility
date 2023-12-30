33e4dda261c16b8193f6dc6be46f0a64df03b043
kavehzare98 <66182975+kavehzare98@users.noreply.github.com>
2023-12-30 09:56:47 -0800

    Major Update

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
-import download_functions as df
+import download_functions as down_funcs
+import documents_functions as docs_funcs
 import os
 from pprint import pprint as visualize
 
@@ -9,25 +10,48 @@ def main():
     while (user_input != 1 and user_input != 2):
         user_input = int(input("Invalid Input! Please choose Downloads (1) or Documents (2):\t"))
 
+    # Cleanup Downloads Folder
     if user_input == 1:
         # Create directories where we want to store the files
         BASE_PATH = os.path.expanduser('~')
+
+        # Create directories where we want to store the files
         DEST_DIRS = ['Music', 'Movies', 'Pictures', 'Documents', 'Executables', 'Other', 'Programming']
 
         # Directories are created only if directory does not exist
-        df.createDirectories(DEST_DIRS, BASE_PATH)
+        down_funcs.createDirectories(DEST_DIRS, BASE_PATH)
 
         # Map files from Downloads folder based on their file extension
         DOWNLOADS_PATH = os.path.join(BASE_PATH, 'Downloads')
 
         # Map files from folder based on their file extension
-        files_map = df.mapFileExtensions(DOWNLOADS_PATH)
+        files_map = down_funcs.mapFileExtensions(DOWNLOADS_PATH)
 
-        df.visualize(files_map)
+        visualize(files_map)
 
-        df.moveFiles(DOWNLOADS_PATH, BASE_PATH, files_map)
-        
+        down_funcs.moveFiles(DOWNLOADS_PATH, BASE_PATH, files_map)
+
+    # Cleanup Documents Folder
     elif user_input == 2:
-        return
+        # Base path where our files are currently located - Documents
+        BASE_PATH = os.path.expanduser('~/Documents')
+
+        # Create directories where we want to store the files
+        DEST_DIRS = ['Text', 'PDF', 'Spreadsheet', 'Docs', 'Presentations', 'Compressed Files']
+
+        # Directories are created only if directory does not exist
+        docs_funcs.createDirectories(DEST_DIRS, BASE_PATH)
+
+        # Map files from Documents folder based on their file extension
+        files_map = docs_funcs.mapFileExtensions(BASE_PATH)
+
+        # Print the initial mapping for visual inspection
+        visualize(files_map)
+
+        # Move files to their respective folders based on file extension
+        docs_funcs.moveFiles(files_map, BASE_PATH)
+
+    else:
+        print("ERROR! Unable to perform requested task.")
 
 main()
